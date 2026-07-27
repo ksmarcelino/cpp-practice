daba158e781f66a276a202edce7a07959a78adc3
ksmarcelino <karlsofianmarcelino@gmail.com>
2026-07-27 20:54:42 +0800

    Done with arithmetic operations practice

---
+++ 01-basic-operations/01-arithmetic.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+        int d, e;
+        
+        cout << "Enter two numbers: ";
+        cin >> d >> e;
+
+
+        cout << "Addition: " << d + e << endl;
+        cout << "Subtraction: " << d - e << endl;
+        cout << "Multiplication: " << d * e << endl;
+        cout << "Division: " << d / e << endl;
+        cout << "Remainder: " << d % e << endl;
+
+        return 0;
+}
