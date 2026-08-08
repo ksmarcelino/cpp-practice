b51b103d03623cc24d200b85f8958e189d300d53
ksmarcelino <karlsofianmarcelino@gmail.com>
2026-08-08 19:55:10 +0800

    Completed functions chapter with Guess the Number project

---
+++ 02-functions/06-final-project-function.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+int secretNumber = 37;
+
+void showIntro();
+int getGuess();
+void checkGuess(int guess, int secretNumber);
+
+int main()
+{
+    int guess, numAttempts = 0;
+    showIntro();
+    do
+    {
+        guess = getGuess();
+        if (guess <= 100 && guess >= 1)
+        {
+            numAttempts++;
+            checkGuess(guess, secretNumber);
+        }
+        else
+        {
+            cout << "\nInvalid guess! Enter a number from 1 to 100." << endl;
+        }
+
+
+    } while (guess != secretNumber && numAttempts < 7);
+    
+    if (numAttempts == 7 && guess != 37)
+    {
+        cout << "\nGame Over!\nThe secret number was " << secretNumber << "." << endl;
+    }
+    else if (numAttempts > 1)
+    {
+        cout << "\nYou guess the number in " << numAttempts << " attempts!" << endl;
+    }
+    else
+    {
+        cout << "\nYou guess the number in " << numAttempts << " attempt!" << endl;
+    }
+
+
+
+    return 0;
+}
+
+void showIntro()
+{
+    cout << "\n\n========== GUESS THE NUMBER ==========\n\nI'm thinking of a number from 1 to 100.(You got only 7 attempts)" << endl;
+}
+
+// Important function
+int getGuess()
+{
+    int numGuess;
+    cout << "\nEnter your guess: ";
+    cin >> numGuess;
+
+    return numGuess;
+}
+
+void checkGuess(int guess, int secretNumber)
+{
+    if (guess > secretNumber)
+    {
+        cout << "Too high!" << endl;
+    }
+    else if (guess < secretNumber)
+    {
+        cout << "Too low!" << endl;
+    }
+    else
+    {
+        cout << "Correct!" << endl;
+    }
+}
