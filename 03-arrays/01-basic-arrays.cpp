6da39275e2c4fab14f57754c3a0f0313e3ad0495
ksmarcelino <karlsofianmarcelino@gmail.com>
2026-08-14 03:16:07 +0800

    Practiced some basic arrays

--- 03-arrays/01-basic-arrays.cpp
+++ 03-arrays/01-basic-arrays.cpp
@@ -3,46 +3,46 @@
 
 int main()
 {
-    std:: string names[5];
+    std::string names[5];
     int viewChoice;
 
-    std:: cout << "Enter Name #1: ";
-    std:: cin >> names[0];
+    std::cout << "Enter Name #1: ";
+    std::cin >> names[0];
 
-    std:: cout << "Enter Name #2: ";
-    std:: cin >> names[1];
+    std::cout << "Enter Name #2: ";
+    std::cin >> names[1];
 
-    std:: cout << "Enter Name #3: ";
-    std:: cin >> names[2];
+    std::cout << "Enter Name #3: ";
+    std::cin >> names[2];
 
-    std:: cout << "Enter Name #4: ";
-    std:: cin >> names[3];
+    std::cout << "Enter Name #4: ";
+    std::cin >> names[3];
 
-    std:: cout << "Enter Name #5: ";
-    std:: cin >> names[4];
+    std::cout << "Enter Name #5: ";
+    std::cin >> names[4];
 
-    std:: cout << "\nFirst Name: " << names[0] << std:: endl;
-    std:: cout << "\nMidddle Name: " << names[2] << std:: endl;
-    std:: cout << "\nLast Name: " << names[4] << std:: endl;
+    std::cout << "\nFirst Name: " << names[0] << std:: endl;
+    std::cout << "\nMidddle Name: " << names[2] << std:: endl;
+    std::cout << "\nLast Name: " << names[4] << std:: endl;
 
-    std:: cout << "\nChange your middle name: ";
-    std:: cin >> names[2];
+    std::cout << "\nChange your middle name: ";
+    std::cin >> names[2];
 
-    std:: cout << "\nFirst Name: " << names[0] << std:: endl;
-    std:: cout << "\nMiddle Name: " << names[2] << std:: endl;
-    std:: cout << "\nLast Name: " << names[4] << std:: endl;
+    std::cout << "\nFirst Name: " << names[0] << std:: endl;
+    std::cout << "\nMiddle Name: " << names[2] << std:: endl;
+    std::cout << "\nLast Name: " << names[4] << std:: endl;
 
-    std:: cout << "\nWhich position do you want to view? (1-5): ";
-    std:: cin >> viewChoice;
+    std::cout << "\nWhich position do you want to view? (1-5): ";
+    std::cin >> viewChoice;
 
     if (viewChoice >= 1 && viewChoice <= 5)
     {
-        std:: cout << "Name #" << viewChoice
+        std::cout << "Name #" << viewChoice
                 << ": " << names[viewChoice - 1] << '\n';
     }
     else
     {
-        std:: cout << "Invalid Choice!\n";
+        std::cout << "Invalid Choice!\n";
     }
 
     return 0;
@@ -57,10 +57,10 @@ int main()
 
     scores[3] = 80;
 
-    std:: cout << "First Score: " << scores[0] << std:: endl;
-    std:: cout << "Third Score: " << scores[2] << std:: endl;
-    std:: cout << "Last Score: " << scores[4] << std:: endl;
-    std:: cout << "Fourth Score: " << scores[3] << std :: endl;
+    std::cout << "First Score: " << scores[0] << std::endl;
+    std::cout << "Third Score: " << scores[2] << std::endl;
+    std::cout << "Last Score: " << scores[4] << std::endl;
+    std::cout << "Fourth Score: " << scores[3] << std::endl;
 
     return 0;
 }*/
