d337e06e33680621f8d65e8069bebbc0000908e4
ksmarcelino <karlsofianmarcelino@gmail.com>
2026-08-29 04:46:55 +0800

    Completed arrays and functions exercises

--- 03-arrays/04-arrays + functions.cpp
+++ 03-arrays/04-arrays + functions.cpp
@@ -28,7 +28,7 @@ int getTotal(int numbers[], int size)
 
     return total;
 
-}*/
+}
 
 
 #include<iostream>
@@ -52,7 +52,7 @@ int getHighest(int scores[], int size)
 {
     int highest = scores[0];
 
-    for (int i = 0; i < size; i++)
+    for (int i = 1; i < size; i++)
     {
         if (highest < scores[i])
         {
@@ -62,4 +62,84 @@ int getHighest(int scores[], int size)
     }
 
     return highest;
+}
+
+#include <iostream>
+#include <string>
+
+int findStudent(std::string students[], int size, std::string searchStudent);
+
+int main()
+{
+    
+    std::string students[5] = {"Dane", "Ian", "Maria", "Alex", "David"};
+    std::string searchStudent;
+
+    std::cout << "Search student: ";
+    std::cin >> searchStudent;
+
+    int foundIndex = findStudent(students, 5, searchStudent);
+
+    if (foundIndex != -1)
+    {
+        std::cout << "Student found at index " << foundIndex;
+    }
+
+    else
+    {
+        std::cout << "Student not found!";
+    }
+
+    return 0;
+}
+
+int findStudent(std::string students[], int size, std::string searchStudent)
+{
+    int foundIndex = -1;
+
+
+    for (int i = 0; i < size; i++)
+    {
+        if (students[i] == searchStudent)
+        {
+            foundIndex = i;
+            break;
+        }
+
+    }
+
+    return foundIndex;
+}*/
+
+#include <iostream>
+
+void changeNumber(int numbers[], int index, int newNumber);
+
+int main()
+{
+    int numbers[5] = {5, 10, 15, 20, 25};
+
+    int indexChange, newNumber;
+
+    std::cout << "Enter index to change: ";
+    std::cin >> indexChange;
+    std::cout << "Enter new number: ";
+    std::cin >> newNumber;
+
+    changeNumber(numbers, indexChange, newNumber);
+
+    for (int i = 0; i < 5; i++)
+    {
+        std::cout << numbers[i] << '\n';
+    }
+
+
+
+
+    return 0;
+}
+
+void changeNumber(int numbers[], int index, int newNumber)
+{
+    numbers[index] = newNumber;
 }
